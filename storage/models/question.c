9f7b275a32c35a56b78135848dd215a3047ae222
MykytaMaliarenko <mykytamaliarenko@gmail.com>
2020-04-05 16:15:15 +0300

    question:  feat: readQuestionFromFile, writeQuestionToFile, questionSetIdCounter  ref: remove readQuestionsFromFile, saveQuestionsToFile

--- storage/models/question.c
+++ storage/models/question.c
@@ -3,10 +3,104 @@
 //
 
 #include "question.h"
+#include <stdlib.h>
+#include "unistd.h"
+#include "string.h"
+#include "../storage.h"
+#include "../storage_fields.h"
 
-typedef struct {
+struct Question {
     int id;
     char* text;
     float levelOfDifficulty;
-} Question;
+};
 
+static int idCounter = 0;
+
+
+QuestionPtr createQuestion(char* text, float levelOfDifficulty)
+{
+    QuestionPtr q = malloc(sizeof(struct Question));
+    q->id = idCounter;
+    q->text = text;
+    q->levelOfDifficulty = levelOfDifficulty;
+
+    idCounter++;
+    return q;
+}
+
+void questionSetIdCounter(int val)
+{
+    idCounter = val;
+}
+
+int questionGetId(QuestionPtr question)
+{
+    return question->id;
+}
+
+char* questionGetText(QuestionPtr question)
+{
+    return question->text;
+}
+
+bool questionSetText(QuestionPtr question, char* text)
+{
+    if (question->text != NULL)
+        free(question->text);
+
+    question->text = text;
+    return true;
+}
+
+float questionGetLevelOfDifficulty(QuestionPtr question)
+{
+    return question->levelOfDifficulty;
+}
+
+bool questionSetLevelOfDifficult(QuestionPtr question, float levelOfDifficulty)
+{
+    if (levelOfDifficulty >=0 && levelOfDifficulty <=100)
+    {
+        question->levelOfDifficulty = levelOfDifficulty;
+        return true;
+    }
+    else
+        return false;
+}
+
+void destroyQuestion(QuestionPtr question)
+{
+    free(question->text);
+    free(question);
+}
+
+bool writeQuestionToFile(FILE *fp, QuestionPtr question)
+{
+    int res = fprintf(fp,"%llu\n%d;%s;%f\n",
+                      strlen(question->text),
+                      question->id,
+                      question->text,
+                      question->levelOfDifficulty);
+
+    if (res > 0)
+        return true;
+    else
+        return false;
+}
+
+QuestionPtr readQuestionFromFile(FILE *fp)
+{
+    QuestionPtr question = calloc(1, sizeof(QuestionPtr));
+
+    unsigned long long strLen;
+    fscanf(fp, "%llu\n", &strLen);
+    question->text = calloc(strLen, sizeof(char));
+
+    fscanf(fp, "%d;%[^;]%*c%f\n",
+           &question->id,
+           question->text,
+           &question->levelOfDifficulty);
+
+    return question;
+}
--- storage/models/question.h
+++ storage/models/question.h
@@ -4,23 +4,29 @@
 
 #ifndef EXAMTASKSBUILDER_QUESTION_H
 #define EXAMTASKSBUILDER_QUESTION_H
+#include <stdbool.h>
+#include <stdio.h>
 
 typedef struct Question* QuestionPtr;
 
 QuestionPtr createQuestion(char* text, float levelOfDifficulty);
 
+int questionGetId(QuestionPtr question);
+
+void questionSetIdCounter(int val);
+
 char* questionGetText(QuestionPtr question);
 
-int questionSetText(QuestionPtr question, char* text);
+bool questionSetText(QuestionPtr question, char* text);
 
 float questionGetLevelOfDifficulty(QuestionPtr question);
 
-int questionSetLevelOfDifficult(QuestionPtr question, float levelOfDifficulty);
+bool questionSetLevelOfDifficult(QuestionPtr question, float levelOfDifficulty);
 
 void destroyQuestion(QuestionPtr question);
 
-QuestionPtr* readQuestionsFromFile(char* filePath);
+bool writeQuestionToFile(FILE *fp, QuestionPtr question);
 
-int saveQuestionsToFile(char* filePath, QuestionPtr* questions);
+QuestionPtr readQuestionFromFile(FILE *fp);
 
 #endif //EXAMTASKSBUILDER_QUESTION_H
