3c31d2c90321b8cb500507a6ebdcc2334315a2f4
Katekisiv <katerinakisiv@gmail.com>
2020-04-15 01:41:22 -0600

    feat event_bus: add event_bus

--- event_bus/event_bus.c
+++ event_bus/event_bus.c
@@ -2,4 +2,86 @@
 // Created by Mykyta Maliarenko on 28.03.2020.
 //
 
+#define EVENT_BUS_MAX_SIZE 100
+#define EVENT_BUS_FIELD_MAX_LISTENERS 20
+
 #include "event_bus.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+typedef struct {
+    int fieldId;
+    listenerFunc listeners[EVENT_BUS_FIELD_MAX_LISTENERS];
+} EventBusField;
+
+static EventBusField* myEventBus[EVENT_BUS_MAX_SIZE] = { 0 };
+
+bool emitEvent(int eventId)
+{
+    for (int i = 0; i < EVENT_BUS_MAX_SIZE; i++)
+    {
+        if (myEventBus[i]->fieldId != NULL && myEventBus[i]->fieldId == eventId)
+        {
+            for (int j = 0; j < EVENT_BUS_FIELD_MAX_LISTENERS; j++)
+            {
+                if (myEventBus[i]->listeners[j] != NULL)
+                    myEventBus[i]->listeners[j](NULL);
+            }
+            return true;
+        }
+    }
+
+    return false;
+}
+
+bool emitEventWithParam(int eventId, void* param)
+{
+    for (int i = 0; i < EVENT_BUS_MAX_SIZE; i++)
+    {
+        if (myEventBus[i]->fieldId != NULL && myEventBus[i]->fieldId == eventId)
+        {
+            for (int j = 0; j < EVENT_BUS_FIELD_MAX_LISTENERS; j++)
+            {
+                if (myEventBus[i]->listeners[j] != NULL)
+                    myEventBus[i]->listeners[j](param);
+            }
+            return true;
+        }
+    }
+
+    return false;
+}
+
+int registerListener(int eventId, listenerFunc listener)
+{
+    for (int i = 0; i < EVENT_BUS_MAX_SIZE; i++)
+    {
+        if (myEventBus[i]->fieldId != NULL && myEventBus[i]->fieldId == eventId)
+        {
+            for (int j = 0; j < EVENT_BUS_FIELD_MAX_LISTENERS; j++)
+            {
+                if (myEventBus[i]->listeners[j] == NULL)
+                {
+                    myEventBus[i]->listeners[j] = listener;
+                    return j;
+                }
+            }
+        }
+    }
+
+    return -1;
+}
+
+bool unregisterListener(int eventId, int listenerId)
+{
+    for (int i = 0; i < EVENT_BUS_MAX_SIZE; i++)
+    {
+        if (myEventBus[i]->fieldId != NULL && myEventBus[i]->fieldId == eventId)
+        {
+            myEventBus[i]->listeners[listenerId] = NULL;
+            return true;
+        }
+    }
+
+    return false;
+}
--- event_bus/event_bus.h
+++ event_bus/event_bus.h
@@ -2,17 +2,18 @@
 // Created by Mykyta Maliarenko on 28.03.2020.
 //
 
+#include <stdbool.h>
 #ifndef EXAMTASKSBUILDER_EVENT_BUS_H
 #define EXAMTASKSBUILDER_EVENT_BUS_H
 
-typedef struct EventBus* EventBusPtr;
+typedef void (*listenerFunc)(void*);
 
-EventBusPtr eventBusGetInstance();
+bool emitEvent(int eventId);
 
-int emitEvent(int eventId);
+bool emitEventWithParam(int eventId, void* param);
 
-int registerListener(int eventId, void (*listener)());
+int registerListener(int eventId, listenerFunc listener);
 
-int unregisterListener(int eventId, int listenerId);
+bool unregisterListener(int eventId, int listenerId);
 
 #endif //EXAMTASKSBUILDER_EVENT_BUS_H
