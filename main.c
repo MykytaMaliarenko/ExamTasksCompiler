b223e773aa26c7aa53357b0294f2eb173e5d8a0b
MykytaMaliarenko <mykytamaliarenko@gmail.com>
2020-04-17 23:12:52 +0300

    main update

--- main.c
+++ main.c
@@ -1,21 +1,9 @@
+#include "core/controllers/main_controller.h"
 #include <gtk/gtk.h>
 
 int main(int argc, char **argv) {
-    GtkBuilder      *builder;
-    GtkWidget       *window;
 
-    gtk_init(&argc, &argv);
-
-    builder = gtk_builder_new();
-    gtk_builder_add_from_file(builder, "D:\\Projects\\C\\ExamTasksCompiler\\gui\\test.glade", NULL);
-
-    window = GTK_WIDGET(gtk_builder_get_object(builder, "window_main"));
-    gtk_builder_connect_signals(builder, NULL);
-
-    g_object_unref(builder);
-
-    gtk_widget_show(window);
-    gtk_main();
-
+    gtk_init (&argc, &argv);
+    startMainWindow();
     return 0;
 }
