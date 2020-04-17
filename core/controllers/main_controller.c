172d864f436dc44bf7c99e4c7563830fa6e14a50
MykytaMaliarenko <mykytamaliarenko@gmail.com>
2020-04-17 23:11:15 +0300

    main_controller

--- core/controllers/main_controller.c
+++ core/controllers/main_controller.c
@@ -3,3 +3,48 @@
 //
 
 #include "main_controller.h"
+#include <stdlib.h>
+#include "../views/main_window.h"
+#include "../../utils/linked_list/linked_list.h"
+#include "../models/questions.h"
+#include "../../event_bus/event_bus.h"
+#include "../../storage/storage.h"
+#include "../../storage/storage_fields.h"
+#include "../../event_bus/events.h"
+#include "add_question_controller.h"
+
+void initEventsListeners();
+
+void addQuestion();
+
+void exportQuestionsToPDF();
+
+
+int startMainWindow()
+{
+    Questions questions = createQuestions();
+    listAdd(questions,createQuestion("test 1", 50));
+    listAdd(questions,createQuestion("test 2", 100));
+
+    storageAdd(STORAGE_QUESTIONS, questions);
+
+    initEventsListeners();
+
+    GtkWidget* window = getMainWindow();
+    gtk_widget_show(window);
+    gtk_main();
+
+    return 0;
+}
+
+void initEventsListeners()
+{
+    eventBusRegisterListener(EVENT_MAIN_WINDOW_ADD_QUESTION, addQuestion);
+
+    //registerListener(EVENT_MAIN_WINDOW_EXPORT_QUESTIONS_TO_PDF, exportQuestionsToPDF);
+}
+
+void addQuestion()
+{
+    showAddQuestionDialog();
+}
